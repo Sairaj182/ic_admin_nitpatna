346faca48f820c160daea31ef23b1ed034e38f8b
Shreyash Kumar <168672263+shreyashkumar01@users.noreply.github.com>
2025-07-16 09:22:16 +0530

    Add files via upload

---
+++ user.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+const bcrypt = require('bcryptjs');
+
+const User = sequelize.define('User', {
+    username: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+// Hash password before creating user
+User.beforeCreate(async (user) => {
+    user.password = await bcrypt.hash(user.password, 10);
+});
+
+// Instance method for password check
+User.prototype.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = User;
