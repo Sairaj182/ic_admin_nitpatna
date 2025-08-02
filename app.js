f973beff7fa01f51ffaa7355275165ba565680ba
Shreyash Kumar <168672263+shreyashkumar01@users.noreply.github.com>
2025-08-02 16:08:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv');
-const sequelize = require('./db');
-const authRoutes = require('./authRoutes');
+const sequelize = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
 // const galleryRoutes = require('./routes/galleryRoutes');
 // const talkRoutes = require('./routes/talkRoutes');
-const noticeRoutes = require('./noticeRoutes');
+// const noticeRoutes = require('./routes/noticeRoutes');
 // const incubationRoutes = require('./routes/incubationRoutes');
 
-dotenv.config();
+require('dotenv').config();
+
+const createAdminUser = require('./initAdmin');
 
 const app = express();
 app.use(cors());
@@ -17,15 +18,16 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 // app.use('/api/gallery', galleryRoutes);
 // app.use('/api/talks', talkRoutes);
-app.use('/api/notices', noticeRoutes);
+// app.use('/api/notices', noticeRoutes);
 // app.use('/api/incubations', incubationRoutes);
 
 app.get('/', (req, res) => {
     res.send('IC-NITP Protected CRUD Backend Running...');
 });
 
-sequelize.sync({ alter: true }).then(() => {
+sequelize.sync().then(() => {
     app.listen(process.env.PORT || 5000, () => {
-        console.log(`🚀 Server running on port ${process.env.PORT || 5000}`);
+         console.log(`🚀 Server running on port ${process.env.PORT}`);
     });
 });
+
